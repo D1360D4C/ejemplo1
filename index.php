c01af318351ffe248bf6cd51f639ff5a2e963c82
D1360D4C <diegoarmando117@gmail.com>
2023-05-27 15:52:56 -0300

    fondo con movimiento

--- index.php
+++ index.php
@@ -5,25 +5,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/gg6easy.css">
+    <link rel="stylesheet" href="pepito.css">
     <title>Ejemplo1</title>
 </head>
 <body>
 
-    <form action="conexion.php" method="post">
-        <input type="text" name="nom" placeholder="Nombre">
-        <input type="text" name="ape" placeholder="Apellido">
-        <button type="submit" name="enviar">Enviar</button>
+    <div class="formulario">
+        <div class= "enviar">
 
-    </form>
+        <form action="conexion.php" method="post">
+            <input type="text" name="nom" placeholder="Nombre">
+            <input type="text" name="ape" placeholder="Apellido">
+            <button type="submit" name="enviar">Enviar</button>
+            
+        </form>
+        </div>
 
-    <form action="eliminar.php" method="post">
-        <input type="text" name="nombre1" placeholder="Nombre">
-        <input type="text" name="apellido" placeholder="Apellido">
-        <button type="submit" name="borrar">Borrar</button>
+        <div class= "borrar">
+            <form action="eliminar.php" method="post">
+            <input type="text" name="nombre1" placeholder="Nombre">
+            <input type="text" name="apellido" placeholder="Apellido">
+            <button type="submit" name="borrar">Borrar</button>
 
-    </form>
+        </form>
+        </div>
 
-
+    </div>
 
     
 </body>
